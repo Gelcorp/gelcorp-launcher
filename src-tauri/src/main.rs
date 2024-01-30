95a6176356c1497935f0c86178049d1190650ba8
MMonkeyKiller <37459591+MMonkeyKiller@users.noreply.github.com>
2024-01-30 01:37:28 -0300

    Move config logic to a separate module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 
 mod msa_auth;
+mod config;
 mod logger;
 mod java;
 mod modpack_downloader;
@@ -18,7 +19,7 @@ use std::{
   collections::VecDeque,
 };
 
-use chrono::Utc;
+use config::{ Authentication, LauncherConfig, MsaMojangAuth };
 use forge_downloader::{
   forge_client_install::ForgeClientInstall,
   download_utils::forge::ForgeVersionHandler,
@@ -27,18 +28,16 @@ use forge_downloader::{
 use log::{ info, debug, error };
 use minecraft_launcher_core::{
   options::{ GameOptionsBuilder, LauncherOptions },
-  profile_manager::auth::{ CommonUserAuthentication, UserAuthentication, OfflineUserAuthentication },
+  profile_manager::auth::UserAuthentication,
   progress_reporter::{ ProgressReporter, ProgressUpdate },
   versions::info::MCVersion,
   MinecraftGameRunner,
 };
-use minecraft_msa_auth::MinecraftAuthorizationFlow;
-use msa_auth::MSAuthToken;
 use once_cell::sync::Lazy;
 use regex::{ Captures, Regex };
 use reqwest::Client;
-use serde::{ Deserialize, Serialize };
-use serde_json::{ json, Value };
+use serde::Serialize;
+use serde_json::json;
 use tauri::{ Window, Manager, Builder, State };
 
 use thiserror::Error;
@@ -55,214 +54,6 @@ const LAUNCHER_VERSION: &str = "2.0.0";
 const GAME_DIR_PATH: Lazy<PathBuf> = Lazy::new(|| resolve_path("%appdata%/.minecraft_test_rust"));
 
 const MINECRAFT_FORGE_VERSION: (&str, &str) = ("1.20.1", "47.2.0");
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(untagged)]
-pub enum Authentication {
-  Offline {
-    username: String,
-    uuid: String,
-  },
-  Msa(MsaMojangAuth),
-}
-
-impl TryInto<Box<dyn UserAuthentication + Send + Sync>> for Authentication {
-  type Error = StdError;
-  fn try_into(self) -> Result<Box<dyn UserAuthentication + Send + Sync>, Self::Error> {
-    match self {
-      Authentication::Msa(msa) => {
-        let auth: CommonUserAuthentication = msa.into();
-        Ok(Box::new(auth))
-      }
-      Authentication::Offline { username, uuid } => Ok(Box::new(OfflineUserAuthentication { username, uuid: Uuid::parse_str(&uuid)? })),
-    }
-  }
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct MsaMojangAuth {
-  username: String,
-  uuid: String,
-
-  moj_token: String,
-  moj_expiration_date: i64,
-
-  msa_access_token: String,
-  msa_refresh_token: String,
-  msa_expiration_date: i64,
-}
-
-impl MsaMojangAuth {
-  fn expired_msa(&self) -> bool {
-    Utc::now().timestamp_millis() > self.msa_expiration_date
-  }
-
-  async fn refresh(&mut self, force: bool) -> Result<(), StdError> {
-    if self.expired_msa() || force {
-      self.refresh_msa(force).await?;
-    }
-    if self.expired_mojang() || force {
-      self.refresh_mojang(force).await?;
-    }
-    self.refresh_profile().await?;
-    Ok(())
-  }
-
-  async fn refresh_msa(&mut self, force: bool) -> Result<(), StdError> {
-    if !self.expired_msa() && !force {
-      return Ok(());
-    }
-    info!("Refreshing Microsoft token...");
-    let mut msa = MSAuthToken {
-      access_token: self.msa_access_token.clone(),
-      refresh_token: self.msa_refresh_token.clone(),
-      expiration_date: self.msa_expiration_date,
-    };
-    msa.refresh(force).await?;
-    self.msa_access_token = msa.access_token;
-    self.msa_refresh_token = msa.refresh_token;
-    self.msa_expiration_date = msa.expiration_date;
-    Ok(())
-  }
-
-  fn expired_mojang(&self) -> bool {
-    Utc::now().timestamp_millis() > self.moj_expiration_date
-  }
-
-  async fn refresh_mojang(&mut self, force: bool) -> Result<(), StdError> {
-    if !self.expired_mojang() && !force {
-      return Ok(());
-    }
-    info!("Refreshing Minecraft token...");
-    let mc_token = MinecraftAuthorizationFlow::new(Client::new())
-      .exchange_microsoft_token(&self.msa_access_token).await
-      .map_err(|err| format!("Failed to exchange msa token: {}", err))?;
-
-    self.username = mc_token.username().clone();
-    self.moj_token = mc_token.access_token().clone().into_inner();
-    self.moj_expiration_date = Utc::now().timestamp_millis() + (mc_token.expires_in() as i64) * 1000;
-    Ok(())
-  }
-
-  async fn refresh_profile(&mut self) -> Result<(), StdError> {
-    info!("Fetching profile info...");
-    let response = Client::new()
-      .get("https://api.minecraftservices.com/minecraft/profile")
-      .bearer_auth(&self.moj_token)
-      .send().await?
-      .error_for_status()?
-      .json::<Value>().await?;
-    self.username = response["name"].as_str().ok_or("Failed to get username")?.to_string();
-    self.uuid = response["id"].as_str().ok_or("Failed to get uuid")?.to_string();
-    info!("Username = {} UUID = {}", self.username, self.uuid);
-    Ok(())
-  }
-
-  async fn from(msa: MSAuthToken) -> Result<Self, StdError> {
-    let mut new = Self {
-      username: String::new(),
-      uuid: String::new(),
-
-      moj_token: String::new(),
-      moj_expiration_date: 0,
-
-      msa_access_token: msa.access_token,
-      msa_refresh_token: msa.refresh_token,
-      msa_expiration_date: msa.expiration_date,
-    };
-    new.refresh(false).await?;
-    Ok(new)
-  }
-}
-
-impl Into<CommonUserAuthentication> for MsaMojangAuth {
-  fn into(self) -> CommonUserAuthentication {
-    CommonUserAuthentication {
-      access_token: self.msa_access_token,
-      auth_playername: self.username,
-      auth_uuid: Uuid::parse_str(&self.uuid).unwrap(),
-      user_type: "msa".to_string(),
-    }
-  }
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-struct LauncherConfig {
-  #[serde(default, skip_serializing_if = "Option::is_none")]
-  authentication: Option<Authentication>,
-
-  #[serde(default, skip_serializing_if = "Vec::is_empty")]
-  selected_options: Vec<String>,
-  #[serde(default = "LauncherConfig::default_providers", skip_serializing_if = "Vec::is_empty")]
-  providers: Vec<String>,
-
-  #[serde(default = "LauncherConfig::default_memory_max")]
-  memory_max: u16,
-}
-
-impl Default for LauncherConfig {
-  fn default() -> Self {
-    Self {
-      authentication: None,
-      selected_options: vec![],
-      providers: LauncherConfig::default_providers(),
-      memory_max: LauncherConfig::default_memory_max(),
-    }
-  }
-}
-
-impl LauncherConfig {
-  fn get_file_path() -> PathBuf {
-    GAME_DIR_PATH.join("launcher_config.json")
-  }
-
-  async fn load_from_file() -> Self {
-    let mut config: LauncherConfig = File::open(Self::get_file_path())
-      .ok()
-      .and_then(|mut f| serde_json::from_reader(&mut f).ok())
-      .unwrap_or_default();
-    config.validate_session().await;
-    let _ = config.save_to_file();
-    config
-  }
-
-  async fn validate_session(&mut self) {
-    if let Some(authentication) = self.authentication.as_mut() {
-      if let Authentication::Msa(msa) = authentication {
-        if let Err(err) = msa.refresh(false).await {
-          error!("Failed to refresh msa token: {}", err);
-          self.authentication = None;
-          let _ = self.save_to_file();
-        } else {
-          info!("Logged in successfully to msa");
-        }
-      }
-    }
-  }
-
-  fn save_to_file(&self) -> Result<(), StdError> {
-    let path = Self::get_file_path();
-    let mut file = File::create(&path)?;
-    if let Some(parent) = path.parent() {
-      create_dir_all(parent)?;
-    }
-    serde_json::to_writer_pretty(&mut file, &self)?;
-    Ok(())
-  }
-
-  fn broadcast_update(&self, window: &Window) -> Result<(), StdError> {
-    window.emit("launcher_config_update", &self)?;
-    Ok(())
-  }
-
-  fn default_memory_max() -> u16 {
-    1024
-  }
-
-  fn default_providers() -> Vec<String> {
-    vec![format!("http://localhost:3000")]
-  }
-}
 
 type StdError = Box<dyn std::error::Error>;
 
