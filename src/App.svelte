db578fa22cf0dd56236af30a5e72ced144a372e6
MMonkeyKiller <37459591+MMonkeyKiller@users.noreply.github.com>
2024-02-06 01:49:50 -0300

    Add UpdatePage

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,28 @@
 <script>
   import MainPage from "$/pages/MainPage.svelte";
   import LoginPage from "$/pages/LoginPage.svelte";
+  import UpdatePage from "$/pages/UpdatePage.svelte";
+  import AlertBoxLayout from "$/components/AlertBoxLayout.svelte";
 
   import { launcherConfigStore } from "$/ipc/stores/launcher_config";
+  import { checkUpdate } from "@tauri-apps/api/updater";
 
   $: authenticated = $launcherConfigStore?.authentication !== undefined;
+
+  let update = checkUpdate();
+  let showUpdateScreen = true;
 </script>
 
-{#if authenticated}
-  <MainPage />
-{:else}
-  <LoginPage />
-{/if}
+{#await update}
+  <AlertBoxLayout>
+    <h4>Buscando actualizaciones...</h4>
+  </AlertBoxLayout>
+{:then { manifest, shouldUpdate }}
+  {#if showUpdateScreen && shouldUpdate}
+    <UpdatePage on:close={() => (showUpdateScreen = false)} {manifest} />
+  {:else if authenticated}
+    <MainPage />
+  {:else}
+    <LoginPage />
+  {/if}
+{/await}
---
+++ src/pages/UpdatePage.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+  import AlertBoxLayout from "$/components/AlertBoxLayout.svelte";
+  import ProgressBar from "$/components/ProgressBar.svelte";
+  import { installUpdate, type UpdateManifest } from "@tauri-apps/api/updater";
+  import { createEventDispatcher } from "svelte";
+
+  export let manifest: UpdateManifest | undefined;
+
+  const dispatch = createEventDispatcher();
+
+  let installing = false;
+  let error: string | undefined;
+
+  async function install() {
+    try {
+      installing = true;
+      await installUpdate();
+    } catch (e) {
+      console.error(e);
+      error = String(e);
+    } finally {
+      installing = false;
+    }
+  }
+
+  function close() {
+    dispatch("close");
+  }
+</script>
+
+<AlertBoxLayout>
+  {#if error === undefined}
+    <header>
+      <img src="gelcorp-title.webp" alt="Logo de Gelcorp" />
+      <h2>Actualización disponible</h2>
+      <p>Es necesario actualizar a la versión <b>{manifest?.version}</b> para continuar</p>
+    </header>
+    <section>
+      <b>Notas de actualización:</b>
+      <code>
+        {manifest?.body}
+      </code>
+    </section>
+    <footer>
+      <button on:click={install} disabled={installing}>Descargar e Instalar</button>
+      {#if installing}
+        <div class="progressbar-container">
+          <ProgressBar progress={-1} --height="6px" />
+        </div>
+      {/if}
+    </footer>
+  {:else}
+    <header>
+      <img src="gelcorp-title.webp" alt="Logo de Gelcorp" />
+    </header>
+    <section class="error">
+      <b>Error al descargar la actualización:</b>
+      <code>{error}</code>
+    </section>
+    <footer>
+      <button on:click={close}>Cerrar</button>
+    </footer>
+  {/if}
+</AlertBoxLayout>
+
+<style>
+  header {
+    text-align: center;
+  }
+
+  header img {
+    height: 2.8rem;
+  }
+
+  section {
+    text-align: left;
+    overflow: hidden;
+  }
+
+  .error code {
+    min-height: auto;
+  }
+
+  code {
+    display: block;
+    background-color: #313131;
+    border-radius: 5px;
+    min-height: 100px;
+    padding: 2px 8px;
+    text-align: left;
+    text-wrap: wrap;
+
+    overflow-y: auto;
+    user-select: text;
+  }
+
+  footer {
+    margin: 5px 0;
+    text-align: center;
+  }
+
+  .progressbar-container {
+    margin: 5px 0;
+  }
+
+  button {
+    padding-block: 8px;
+    width: 100%;
+
+    margin-top: 5px;
+    background-color: #e1e1e1;
+    color: black;
+    border: 1px solid gray;
+
+    box-sizing: border-box;
+  }
+</style>
