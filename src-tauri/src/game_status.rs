541c9ad53f9072d329c02f0fe810d28c0cfc41fe
MMonkeyKiller <37459591+MMonkeyKiller@users.noreply.github.com>
2024-07-15 22:44:47 -0300

    Refactor `game_status.rs`

--- src-tauri/src/game_status.rs
+++ src-tauri/src/game_status.rs
@@ -3,32 +3,36 @@ use std::sync::Mutex;
 use serde::Serialize;
 use tauri::Window;
 
-#[derive(Serialize, Clone)]
+#[derive(Serialize, Clone, Default)]
 pub enum GameStatus {
-  Idle,
+  #[default] Idle,
   Downloading,
   Playing,
 }
 
-pub struct GameStatusState(Mutex<GameStatus>, Mutex<Option<Window>>);
+#[derive(Default)]
+pub struct GameStatusState {
+  status: Mutex<GameStatus>,
+  window: Mutex<Option<Window>>,
+}
 
 impl GameStatusState {
   pub fn new() -> Self {
-    Self(Mutex::new(GameStatus::Idle), Mutex::new(None))
+    Self::default()
   }
 
   pub fn set_window(&self, window: Window) {
-    self.1.lock().unwrap().replace(window);
+    self.window.lock().unwrap().replace(window);
   }
 
   pub fn set(&self, status: GameStatus) {
-    *self.0.lock().unwrap() = status.clone();
-    if let Some(window) = &*self.1.lock().unwrap() {
+    *self.status.lock().unwrap() = status.clone();
+    if let Some(window) = &*self.window.lock().unwrap() {
       let _ = window.emit("game_status", status);
     }
   }
 
   pub fn get(&self) -> GameStatus {
-    self.0.lock().unwrap().clone()
+    self.status.lock().unwrap().clone()
   }
 }
